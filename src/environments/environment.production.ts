46dacf009d45852425153df6385a25527e5a38c1
yuseth <yuseth28124@gmail.com>
2026-02-19 02:53:14 -0500

    first commit 5

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'sistema-de-cursos-production.up.railway.app',
+  apiUrl: '/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'sistema-de-cursos-production.up.railway.app',
+  production: false,
+  apiUrl: 'http://localhost:8080',
 };
